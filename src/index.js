44edb91c450ceed8302c7f3f59d6e75d9ec80d3a
koverchik <koverchik.o@gmail.com>
2020-10-17 15:54:24 +0300

    fix: if no params passed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let newArray = [];
-    for (let i = 0; i < matrix.length; i++) {
-      if(i%2 == 0){
-          newArray.push(matrix[i]);
-      }else {
-          let reverseArray = matrix[i].reverse();
-          newArray.push(reverseArray);
+if(arguments.length == 0){
+  return [];
+}else {
+    let newArray = [];
+      for (let i = 0; i < matrix.length; i++) {
+        if(i%2 == 0){
+            newArray.push(matrix[i]);
+        }else {
+            let reverseArray = matrix[i].reverse();
+            newArray.push(reverseArray);
+        }
       }
-    }
-   return newArray.flat(Infinity);
+     return newArray.flat(Infinity);
+
+
+}
 }
